e9349640afeeb6c1ed3eacbd1d374e969ad66695
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-02-14 12:02:00 +0100

    Add bind() polyfill, required by PhantomJS (ref #597)

--- mapentity/static/mapentity/mapentity.js
+++ mapentity/static/mapentity/mapentity.js
@@ -9,6 +9,29 @@ console.debug = function () {
 };
 
 
+if (!Function.prototype.bind) {
+    Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP && oThis ? this : oThis,
+                               aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+    };
+}
+
+
 if (!MapEntity) var MapEntity = {};
 
 L.Control.Screenshot = L.Control.extend({
@@ -255,7 +278,7 @@ MapEntity.Context = new function() {
             context = self.__loadFullContext(kwargs);
         }
         if (!context) {
-            console.warn("No context found.");
+            console.debug("No context found.");
             map.fitBounds(map.options.maxBounds);
             return;  // No context, no restore.
         }
